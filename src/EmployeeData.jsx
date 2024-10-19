fb53382e1be8910fd82f5aa00993784e9077270e
Rohitkm28 <135746541+Rohitkm28@users.noreply.github.com>
2024-10-19 09:12:14 +0530

    Add files via upload

---
+++ src/EmployeeData.jsx
@@ -0,0 +1,68 @@
+export const EmployeeData = [
+  {
+  id : 1,
+  firstName : "Mark",
+  lastName : "David",
+  age : 30
+  },
+  {
+    id : 2,
+    firstName : "Sam",
+    lastName : "Jhon",
+    age : 40
+    },
+    {
+      id : 3,
+      firstName : "Rohit",
+      lastName : "Mishra",
+      age : 35
+      },
+      {
+        id : 4,
+        firstName : "Mohit",
+        lastName : "Sinha",
+        age : 60
+        },
+        {
+          id : 5,
+          firstName : "Raju",
+          lastName : "Singh",
+          age : 26
+          },
+          {
+            id : 6,
+            firstName : "Ramu",
+            lastName : "Thakur",
+            age : 38
+            },
+            {
+              id : 7,
+              firstName : "Aman",
+              lastName : "Sinha",
+              age : 28
+              },
+              {
+                id : 8,
+                firstName : "Sohan",
+                lastName : "Raj",
+                age : 22
+                },
+                {
+                  id : 9,
+                  firstName : "Dev",
+                  lastName : "Mishra",
+                  age : 27
+                  },
+                  {
+                    id : 10,
+                    firstName : "Raj",
+                    lastName : "Kumar",
+                    age : 29
+                    },
+                    {
+                      id : 7,
+                      firstName : "Mohan",
+                      lastName : "Lal",
+                      age : 82
+                      },
+]
